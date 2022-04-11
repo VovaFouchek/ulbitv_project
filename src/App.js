1c4683e857b8e34f8ccfc143821532c5ea52f959
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-11 14:49:46 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import ClassCounter from './components/ClassCounter';
+import PostForm from "./components/PostForm";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import './styles/App.css'
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ])
+
+  const CreatePost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={CreatePost} />
+      {posts.length
+      ?
+      <PostList remove={removePost} posts={posts} title="Lists of:" />
+      :
+      <h1 style={{textAlign: 'center'}}>Posts not found</h1>
+      }
+     
     </div>
   );
 }
