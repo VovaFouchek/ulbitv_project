2ba448bab425b71f6dca7fadbdc681df98b8cb7e
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-12 11:43:26 +0300

    Added select

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css'
 
 function App() {
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const [posts, setPosts] = useState([
     { id: 1, title: 'Javascript', body: 'Description' },
@@ -23,16 +26,32 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    console.log(sort);
+  }
+
   return (
     <div className="App">
       <PostForm create={CreatePost} />
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
       {posts.length
-      ?
-      <PostList remove={removePost} posts={posts} title="Lists of:" />
-      :
-      <h1 style={{textAlign: 'center'}}>Posts not found</h1>
+        ?
+        <PostList remove={removePost} posts={posts} title="Lists of:" />
+        :
+        <h1 style={{ textAlign: 'center' }}>Posts not found</h1>
       }
-     
     </div>
   );
 }
