f224c48a82fbab84a39535c451ef1b659e018b8a
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-14 13:21:45 +0300

    Added custom hook

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/MyModal/MyModal";
 import './styles/App.css'
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,18 +16,7 @@ function App() {
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    console.log('Отаработала функция сортировки');
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const CreatePost = (newPost) => {
     setPosts([...posts, newPost])
