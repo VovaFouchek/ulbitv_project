3d8bf0477fba643eaf57cf97523174f4637217e1
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-04-14 14:00:52 +0300

    Uploading data from the server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from "react";
+import React, { useState, useRef, useMemo, useEffect } from "react";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
@@ -6,17 +6,29 @@ import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/MyModal/MyModal";
 import './styles/App.css'
 import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-  const [posts, setPosts] = useState([
+  const [posts, setPosts] = useState([/*
     { id: 1, title: 'Java', body: 'This is a Java' },
     { id: 2, title: 'C#', body: 'Some text' },
-    { id: 3, title: 'Python', body: 'DataBase' },
+    { id: 3, title: 'Python', body: 'DataBase' },*/
   ])
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data);
+  }
+
+  useEffect(() => {
+    console.log('Use Effect');
+    fetchPosts();
+  }, [])
+
 
   const CreatePost = (newPost) => {
     setPosts([...posts, newPost])
@@ -29,6 +41,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* <button onClick={fetchPosts}>Get Posts</button> */}
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>Создать пользователя</MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={CreatePost} />
